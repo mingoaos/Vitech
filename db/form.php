21484639c504ede00a98a985510db87a43d6f286
joao <jonnhy695@gmail.com>
2024-05-09 10:33:13 +0100

    as

--- db/form.php
+++ db/form.php
@@ -42,7 +42,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         } else {
             $_SESSION['alert'] = "Error: " . mysqli_error($con);
         }
-        // Redirect back to the index page
+        
         header("Location: ../");
         exit();
     }
